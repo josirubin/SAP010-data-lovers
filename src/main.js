49f69cb3056a0c98156e9ce7fcb717553846eed4
Josielen Rubin <josirubinc@hotmail.com>
2023-05-15 13:27:44 -0300

    inseri os slides da página inicial e input de pesquisa e botão de reset

--- src/main.js
+++ src/main.js
@@ -22,3 +22,22 @@ function criaCardPersonagens(personagens) {
 
     document.querySelector("#card-personagens").innerHTML = printarCard.join("");
 }
+
+
+// PASSAGEM AUTOMATICA SLIDE
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+    nextImage();
+}, 2000)
+
+function nextImage(){
+    count++;
+    if (count > 2) {
+        count = 1
+    }
+
+    document.getElementById("radio"+count).checked = true;
+}
