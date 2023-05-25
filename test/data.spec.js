aff2b4d7658829fbd6301d50e2ce212b186307c4
Josielen Rubin <josirubinc@hotmail.com>
2023-05-25 13:34:40 -0300

    testes das funções aplicados

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,21 +1,21 @@
-import { buscarNome, ordenarAZ, filtrar } from '../src/data.js';
+import { ordenarAZ, filtrar } from '../src/data.js';
 
-const arrayTeste = [
-  {
-    "name": "Rick Sanchez",
-    "status": "Alive",
-    "species": "Human",
-  },
-  {
-    "name": "Morty Smith",
-    "status": "Alive",
-    "species": "Human",
-  },
-  {
-    "name": "Summer Smith",
-    "status": "Alive",
-    "species": "Human",
-  }]
+// const arrayTeste = [
+//   {
+//     "name": "Rick Sanchez",
+//     "status": "Alive",
+//     "species": "Human",
+//   },
+//   {
+//     "name": "Morty Smith",
+//     "status": "Alive",
+//     "species": "Human",
+//   },
+//   {
+//     "name": "Summer Smith",
+//     "status": "Alive",
+//     "species": "Human",
+//   }]
 
 //   it('returns `example`', () => {
 //     expect(example()).toBe('example');
@@ -32,3 +32,16 @@ const arrayTeste = [
 //     expect(anotherExample()).toBe('OMG');
 //   });
 // });
+
+describe('filtrar', () => {
+  it('deve ser uma função', () => {
+    expect(typeof filtrar).toBe('function');
+  })
+})
+
+
+describe('ordenarAZ', () => {
+  it('deve ser uma função', () => {
+    expect(typeof ordenarAZ).toBe('function');
+  })
+})
