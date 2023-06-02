95d2efdc78969a3d29f7c63b29f4e4f2526ab674
Josielen Rubin <josirubinc@hotmail.com>
2023-06-02 13:19:02 -0300

    Todas as funções testadas, passando 100% pelos testes!

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,42 +1,76 @@
-import { ordenarAZ } from '../src/data.js';
+import { ordenarAZ, filtrar, calcularPorcentagem, buscarNome } from '../src/data.js';
 
 const arrayTeste = [{
-  "name": "Rick",
-  "status": "Alive",
+  "name": "Diane Sanchez",
+  "status": "unknown",
   "species": "Human"
 },
 {
-  "name": "Morty",
-  "status": "Alive",
+  "name": "Adjudicator Rick",
+  "status": "Dead",
   "species": "Human"
 },
 {
-  "name": "Summer",
+  "name": "Daron Jefferson",
   "status": "Alive",
-  "species": "Human"
+  "species": "Alien"
 }]
 
+//testa ordenação AZ
 describe('ordenar AZ - ZA', () => {
   it('is a function', () => {
     expect(typeof ordenarAZ).toBe('function');
   });
 
   it('retornar AZ', () => {
-    expect(ordenarAZ('a-z', arrayTeste)).toEqual([{"name": "Morty", "status": "Alive", "species": "Human"}, {"name": "Rick", "status": "Alive", "species": "Human"}, {"name": "Summer", "status": "Alive", "species": "Human"}]);
+    expect(ordenarAZ('a-z', arrayTeste)).toEqual([{"name": "Adjudicator Rick", "status": "Dead", "species": "Human"}, {"name": "Daron Jefferson", "status": "Alive", "species": "Alien"}, {"name": "Diane Sanchez", "status": "unknown", "species": "Human"}]);
   
-    expect(ordenarAZ('a-z', arrayTeste)[0].name).toBe('Morty');
+    expect(ordenarAZ('a-z', arrayTeste)[0].name).toBe('Adjudicator Rick');
+  });
+});
+
+//testa ordenação ZA
+describe('ordenar AZ - ZA', () => {
+  it('is a function', () => {
+    expect(typeof ordenarAZ).toBe('function');
   });
 
+  it('retornar ZA', () => {
+    expect(ordenarAZ('z-a', arrayTeste)).toEqual([{"name": "Diane Sanchez", "status": "unknown", "species": "Human"}, {"name": "Daron Jefferson", "status": "Alive", "species": "Alien"}, {"name": "Adjudicator Rick", "status": "Dead", "species": "Human"}]);
+  });
+});
 
+//testa filtro por status
+describe('Filtrar por Status', () => {
+  it('is a function', () => {
+    expect(typeof filtrar).toBe('function');
+  });
+
+  it('retornar os personagens por status', () => {
+    expect(filtrar(arrayTeste, 'unknown', "status")).toEqual([{"name": "Diane Sanchez", "status": "unknown", "species": "Human"}]);
+    expect(filtrar(arrayTeste, 'alive', "status")).toEqual([{"name": "Daron Jefferson", "status": "Alive", "species": "Alien"}]);
+    expect(filtrar(arrayTeste, 'dead', "status")).toEqual([{"name": "Adjudicator Rick", "status": "Dead", "species": "Human"}]);
+  });
+});
+
+//testa o cálculo
+describe('Calculo agregado', () => {
+  it('is a function', () => {
+    expect(typeof calcularPorcentagem).toBe('function');
+  });
+
+  it('retornar o cálculo de porcentagem referente a human', () => {
+    expect(calcularPorcentagem(3, 2)).toBe(67)
+  })
 });
 
 
-// describe('anotherExample', () => {
-//   it('is a function', () => {
-//     expect(typeof anotherExample).toBe('function');
-//   });
+describe('Busca por nome', () => {
+  it('is a function', () => {
+    expect(typeof buscarNome).toBe('function');
+  });
 
-//   it('returns `anotherExample`', () => {
-//     expect(anotherExample()).toBe('OMG');
-//   });
-// });
+  it('busca por nome', () => {
+    expect(buscarNome(arrayTeste, 'Diane')).toEqual([{"name": "Diane Sanchez", "status": "unknown", "species": "Human"}])
+  })
+})
